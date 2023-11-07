1d8aa6718390c3189e30f9aa6146e03886c9b6a3
ItsMeSamG <itsmesamg123@gmail.com>
2023-11-07 17:23:02 -0500

    ProgressION

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,15 +1,39 @@
-
-
-
+import PRODUCT_DATA from "../data/PRODUCT_DATA";
+const { ROLL_TYPES } = PRODUCT_DATA;
 
 function Viewer() {
   return (
     <div className="viewer">
-      <div className="viewer--controls"></div>
+      <div className="viewer--sidebar">
+        <Title />
+
+        <div className="viewer--sidebar-panel viewer--sidebar-button">
+          <h2 className="viewer--h2">Traditional Pre-Rolls</h2>
+        </div>
+        <div className="viewer--sidebar-panel viewer--sidebar-button">
+          <h2 className="viewer--h2">Cigarette Pre-Rolls</h2>
+        </div>
+      </div>
 
       <div className="viewer--content"></div>
     </div>
   );
 }
+
+function Title() {
+  return (
+    <div className="viewer--title viewer--sidebar-panel">
+      <h1 className="viewer--h1 viewer__underline">Pre-Rolls</h1>
+    </div>
+  );
+}
+
+// function Viewer() {
+//     return (
+//         <div>
+//             Enter
+//         </div>
+//     );
+// }
 
 export default Viewer;
---
+++ src/data/PRODUCT_DATA.js
@@ -0,0 +1,41 @@
+const PRODUCT_DATA = {
+ 
+  
+  ROLL_TYPES: {
+
+    title: "Pre-Rolls",
+    types: [
+        {
+            id: "traditional",
+            index: 1,
+            short: "Traditional",
+            long: "Traditional Pre-Roll",
+            image: "",
+        }, 
+        {
+            id: "cigarette",
+            index: 2,
+            short: "Cigarette",
+            long: "Cigarette Pre-Roll",
+            image: "",
+        },
+        {
+            id: "blunt",
+            index: 3,
+            short: "Blunt",
+            long: "Blunt",
+            image: "",
+        },
+        {
+            id: "infused",
+            index: 4,
+            short: "Infused",
+            long: "Infused Pre-Roll",
+            image: "",
+        },
+        ],
+
+  },
+};
+
+export default PRODUCT_DATA;
