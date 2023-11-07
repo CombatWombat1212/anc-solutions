24aff478d542c8ffc3d1c11a6242bc20f1aefecd
ItsMeSamG <itsmesamg123@gmail.com>
2023-11-07 14:22:40 -0500

    why

--- my-react-boilerplate/src/App.js
+++ my-react-boilerplate/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Check check
+          test
         </p>
         <a
           className="App-link"
