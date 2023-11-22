d489a337ec02d4ac7e306e4f411def4c878b7685
ItsMeSamG <itsmesamg123@gmail.com>
2023-11-22 17:02:55 -0500

    hows this

--- src/pages/Sizes.js
+++ src/pages/Sizes.js
@@ -5,18 +5,27 @@ import SIZES_IMGS from "../data/SIZES_IMGS";
 
 import SVG from "react-inlinesvg";
 import { useGraphicLoadManager, useGraphicLoadTracker } from "../scripts/hooks/useGraphicLoadManager";
+import { useSpring, useTransition, animated } from '@react-spring/web';
+
+
 
 function Sizes({ view }) {
   const imgs = Object.values(SIZES_IMGS[view.type]);
 
   const { graphicContainerProps } = useGraphicLoadManager(view, { count: imgs.length });
 
+
+  useEffect(() => {
+    console.log(view.pageLoading);
+  }, [view.pageLoading]);
+
+  
   return (
     <>
       <div className="sizes--body">
-        {imgs.map((img, i) => {
-          return <Item imgs={imgs} index={i} key={i} {...graphicContainerProps} />;
-        })}
+      {imgs.map((img, i) => {
+  return <Item imgs={imgs} index={i} key={i} view={view} {...graphicContainerProps} />;
+})}
         <div className="sizes--background">
           <Label1 className="sizes--background-label sizes--background-label__x">Grams</Label1>
           <Label1 className="sizes--background-label sizes--background-label__y">Length</Label1>
@@ -26,16 +35,23 @@ function Sizes({ view }) {
   );
 }
 
-function Item({ imgs, index, ...props }) {
+function Item({ imgs, index, view, ...props }) {
   const img = imgs[index];
   const columns = imgs.length;
 
   const { graphicElementProps } = useGraphicLoadTracker(props, { index: index });
 
+  const springStyle = useSpring({
+    from: { opacity: 0, transform: 'translateX(-1rem)' },
+    to: { opacity: view.pageReady ? 1 : 0, transform: view.pageReady ? 'translateX(0)' : 'translateX(-1rem)' },
+    delay: index * 100, // Delay each item slightly
+  });
+
   return (
-    <div
+    <animated.div
       className="sizes--col"
       style={{
+        ...springStyle,
         "--img-src": img.src,
         "--sizes-columns": columns,
         "--graphic-width": img.width,
@@ -43,7 +59,7 @@ function Item({ imgs, index, ...props }) {
       }}>
       <SVG className={`sizes--graphic`} src={img.src} width={img.width} height={img.height} {...graphicElementProps} />
       <Brace img={img} />
-    </div>
+    </animated.div>
   );
 }
 
--- src/scripts/contexts/viewProvider.js
+++ src/scripts/contexts/viewProvider.js
@@ -1,52 +1,63 @@
 // ViewProvider.js
-import React, { useState, useRef, useEffect } from 'react';
-import { ViewContext } from './viewContext';
+import React, { useState, useRef, useEffect } from "react";
+import { ViewContext } from "./viewContext";
 
 const ViewProvider = ({ children }) => {
+  const [hoveredSideBtn, setHoveredSideBtn] = useState(false);
+  const [page, setPage] = useState("selection");
+  const [pageType, setPageType] = useState(false);
+  const [dockType, setDockType] = useState(false);
+  const [dockStats, setDockStats] = useState(false);
+  const [dockActiveObj, setDockActiveObj] = useState(false);
+  const pageRef = useRef(null);
+  const [pageLoading, setPageLoading] = useState(true);
+  const [pageReady, setPageReady] = useState(false);
+  const [previousPage, setPreviousPage] = useState({ page: "selection", type: false });
 
-    const [hoveredSideBtn, setHoveredSideBtn] = useState(false);
-    const [page, setPage] = useState("selection");
-    const [pageType, setPageType] = useState(false);
-    const [dockType, setDockType] = useState(false);
-    const [dockStats, setDockStats] = useState(false);
-    const [dockActiveObj, setDockActiveObj] = useState(false);
-    const pageRef = useRef(null);
-    const [pageLoading, setPageLoading] = useState(true);
-    const [previousPage, setPreviousPage] = useState({page: "selection", type: false});
+  useEffect(() => {
+    let timeoutId;
 
-    const view = {
-
-        page: page,
-        setPage: setPage,
-        type: pageType,
-        setType: setPageType,
-        dock: dockType,
-        setDock: setDockType,
-        dockStats: dockStats,
-        setDockStats: setDockStats,
-        dockActiveObj: dockActiveObj,
-        setDockActiveObj: setDockActiveObj,
-
-        pageLoading: pageLoading,
-        setPageLoading: setPageLoading,
-
-        // hoveredSideBtn: hoveredSideBtn,
-        pageRef: pageRef,
-        
-        side:{
-            active: hoveredSideBtn,
-            setActive: setHoveredSideBtn,
-        },
-    
+    if (pageLoading) {
+        // Set pageReady to true after 250ms if pageLoading is true
+        timeoutId = setTimeout(() => setPageReady(true), 250);
+    } else if (!pageLoading && pageReady) {
+        // Set pageReady to false after 250ms if pageLoading is false
+        timeoutId = setTimeout(() => setPageReady(false), 250);
     }
 
+    return () => clearTimeout(timeoutId); // Cleanup the timeout if the component unmounts
+}, [pageLoading]); // Dependency array
 
 
-    return (
-        <ViewContext.Provider value={view}>
-            {children}
-        </ViewContext.Provider>
-    );
+
+  const view = {
+    page: page,
+    setPage: setPage,
+    type: pageType,
+    setType: setPageType,
+    dock: dockType,
+    setDock: setDockType,
+    dockStats: dockStats,
+    setDockStats: setDockStats,
+    dockActiveObj: dockActiveObj,
+    setDockActiveObj: setDockActiveObj,
+
+    pageLoading: pageLoading,
+    setPageLoading: setPageLoading,
+
+    pageReady: pageReady,
+    setPageReady: setPageReady,
+
+    // hoveredSideBtn: hoveredSideBtn,
+    pageRef: pageRef,
+
+    side: {
+      active: hoveredSideBtn,
+      setActive: setHoveredSideBtn,
+    },
+  };
+
+  return <ViewContext.Provider value={view}>{children}</ViewContext.Provider>;
 };
 
 export default ViewProvider;
